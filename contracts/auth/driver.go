b9448ed5dfd9a46a511eca9ec99162fde040ec80
Evgeny <113383200+KlassnayaAfrodita@users.noreply.github.com>
2025-01-27 00:53:54 +0300

    Update driver.go

--- contracts/auth/driver.go
+++ contracts/auth/driver.go
@@ -3,5 +3,5 @@ package auth
 type Driver interface {
     Login(userID string, data map[string]interface{}) (string, error)    // User login
     Logout(sessionID string) error                                      // User logout
-    Authenticate(sessionID string) (User, error)                        // Check authentication
+    Authenticate(sessionID string) error                        // Check authentication
 }
